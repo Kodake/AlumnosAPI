18d265c2874918126809b2ce8ab35fd5c5518ead
DESKTOP-O5BLINM\Admin <rflorentino@wepsys.net>
2020-09-29 23:32:56 -0400

    All changes

---
+++ BackEnd/Models/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Alumno
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+    }
+}
